ed1d0bf97cb36d01c7a267b1260aefd332dba68f
John Finamore <jhfinamore@gmail.com>
2018-12-04 10:45:00 -0500

    Adding the python files

---
+++ 408Final2.py
@@ -0,0 +1,384 @@
+import random
+
+def letterToNavajo1(a):
+    if(a == "a"):
+        return "WOL-LA-CHEE"
+    if(a == "b"):
+        return "NA-HASH-CHID"
+    if(a == "c"):
+        return "MOASI"
+    if(a == "d"):
+        return "BE"
+    if(a == "e"):
+        return "AH-JAH"
+    if(a == "f"):
+        return "CHUO"
+    if(a == "g"):
+        return "AH-TAD"
+    if(a == "h"):
+        return "TSE-GAH"
+    if(a == "i"):
+        return "TKIN"
+    if(a == "j"):
+        return "TKELE-CHO-G"
+    if(a == "k"):
+        return "JAD-HO-LONI"
+    if(a == "l"):
+        return "DIBEH-YAZZIE"
+    if(a == "m"):
+        return "TSIN-TLITI"
+    if(a == "n"):
+        return "TSAH"
+    if(a == "o"):
+        return "A-KHA"
+    if(a == "p"):
+        return "CLA-GI-AIH"
+    if(a == "q"):
+        return "CA-YEILTH"
+    if(a == "r"):
+        return "GAH"
+    if(a == "s"):
+        return "DIBEH"
+    if(a == "t"):
+        return "D-AH"
+    if(a == "u"):
+        return "SHI-DA"
+    if(a == "v"):
+        return "A-KEH-DI-GLINI"
+    if(a == "w"):
+        return "GLOE-IH"
+    if(a == "x"):
+        return "AL-NA-AS-DZOH"
+    if(a == "y"):
+        return "TSAH-AS-ZIH"
+    if(a == "z"):
+        return "BESH-DO-TLIZ"
+    if(a==" "):
+    	return ""
+        
+def letterToNavajo2(a):
+    if(a == "a"):
+        return "BE-LA-SANA"
+    if(a == "b"):
+        return "SHUSH"
+    if(a == "c"):
+        return "TLA-GIN"
+    if(a == "d"):
+        return "CHINDI"
+    if(a == "e"):
+        return "DZEH"
+    if(a == "f"):
+        return "TSA-E-DONIN-EE"
+    if(a == "g"):
+        return "KLIZZIE"
+    if(a == "h"):
+        return "CHA"
+    if(a == "i"):
+        return "YEH-HES"
+    if(a == "j"):
+        return "AH-YA-TSINNE"
+    if(a == "k"):
+        return "BA-AH-NE-DI-TININ"
+    if(a == "l"):
+        return "AH-JAD"
+    if(a == "m"):
+        return "BE-TAS-TNI"
+    if(a == "n"):
+        return "A-CHIN"
+    if(a == "o"):
+        return "TLO-CHIN"
+    if(a == "p"):
+        return "BI-SO-DIH"
+    if(a == "q"):
+        return "CA-YEILTH"
+    if(a == "r"):
+        return "DAH-NES-TSA"
+    if(a == "s"):
+        return "KLESH"
+    if(a == "t"):
+        return "A-WOH"
+    if(a == "u"):
+        return "NO-DA-IH"
+    if(a == "v"):
+        return "A-KEH-DI-GLINI"
+    if(a == "w"):
+        return "GLOE-IH"
+    if(a == "x"):
+        return "AL-NA-AS-DZOH"
+    if(a == "y"):
+        return "TSAH-AS-ZIH"
+    if(a == "z"):
+        return "BESH-DO-TLIZ"
+    if(a==" "):
+    	return ""
+        
+def letterToNavajo3(a):
+    if(a == "a"):
+        return "TSE-NILL"
+    if(a == "b"):
+        return "TOISH-JEH"
+    if(a == "c"):
+        return "BA-GOSHI"
+    if(a == "d"):
+        return "LHA-CHA-EH"
+    if(a == "e"):
+        return "AH-NAH"
+    if(a == "f"):
+        return "MA-E"
+    if(a == "g"):
+        return "JEHA"
+    if(a == "h"):
+        return "LIN"
+    if(a == "i"):
+        return "A-CHI"
+    if(a == "j"):
+        return "YIL-DOI"
+    if(a == "k"):
+        return "KLIZZIE-YAZZIE"
+    if(a == "l"):
+        return "NASH-DOIE-TSO"
+    if(a == "m"):
+        return "NA-AS-TSO-SI"
+    if(a == "n"):
+        return "TSAH"
+    if(a == "o"):
+        return "NE-AHS-JAH"
+    if(a == "p"):
+        return "NE-ZHONI"
+    if(a == "q"):
+        return "CA-YEILTH"
+    if(a == "r"):
+        return "AH-LOSZ"
+    if(a == "s"):
+        return "DIBEH"
+    if(a == "t"):
+        return "THAN-ZIE"
+    if(a == "u"):
+        return "SHI-DA"
+    if(a == "v"):
+        return "A-KEH-DI-GLINI"
+    if(a == "w"):
+        return "GLOE-IH"
+    if(a == "x"):
+        return "AL-NA-AS-DZOH"
+    if(a == "y"):
+        return "TSAH-AS-ZIH"
+    if(a == "z"):
+        return "BESH-DO-TLIZ"
+    if(a==" "):
+    	return ""
+
+def letterToEnglish1(a):
+    if(a == "a"):
+        return "Ant"
+    if(a == "b"):
+        return "Badger"
+    if(a == "c"):
+        return "Cat"
+    if(a == "d"):
+        return "Deer"
+    if(a == "e"):
+        return "Ear"
+    if(a == "f"):
+        return "Fir"
+    if(a == "g"):
+        return "Girl"
+    if(a == "h"):
+        return "Hair"
+    if(a == "i"):
+        return "Ice"
+    if(a == "j"):
+        return "JackAss"
+    if(a == "k"):
+        return "Kettle"
+    if(a == "l"):
+        return "Lamb"
+    if(a == "m"):
+        return "Match"
+    if(a == "n"):
+        return "Needle"
+    if(a == "o"):
+        return "Oil"
+    if(a == "p"):
+        return "Pant"
+    if(a == "q"):
+        return "Quiver"
+    if(a == "r"):
+        return "Rabbit"
+    if(a == "s"):
+        return "Sheep"
+    if(a == "t"):
+        return "Tea"
+    if(a == "u"):
+        return "Uncle"
+    if(a == "v"):
+        return "Victor"
+    if(a == "w"):
+        return "Weasel"
+    if(a == "x"):
+        return "Cross"
+    if(a == "y"):
+        return "Yucca"
+    if(a == "z"):
+        return "Zinc"
+    if(a==" "):
+    	return ""
+
+def letterToEnglish2(a):
+    if(a == "a"):
+        return "Apple"
+    if(a == "b"):
+        return "Bear"
+    if(a == "c"):
+        return "Coal"
+    if(a == "d"):
+        return "Devil"
+    if(a == "e"):
+        return "Elk"
+    if(a == "f"):
+        return "Fly"
+    if(a == "g"):
+        return "Goat"
+    if(a == "h"):
+        return "Hat"
+    if(a == "i"):
+        return "Itch"
+    if(a == "j"):
+        return "Jaw"
+    if(a == "k"):
+        return "Key"
+    if(a == "l"):
+        return "Leg"
+    if(a == "m"):
+        return "Mirror"
+    if(a == "n"):
+        return "Nose"
+    if(a == "o"):
+        return "Onion"
+    if(a == "p"):
+        return "Pig"
+    if(a == "q"):
+        return "Quiver"
+    if(a == "r"):
+        return "Ram"
+    if(a == "s"):
+        return "Snake"
+    if(a == "t"):
+        return "Tooth"
+    if(a == "u"):
+        return "Ute"
+    if(a == "v"):
+        return "Victor"
+    if(a == "w"):
+        return "Weasel"
+    if(a == "x"):
+        return "Cross"
+    if(a == "y"):
+        return "Yucca"
+    if(a == "z"):
+        return "Zinc"
+    if(a==" "):
+    	return "" 
+
+def letterToEnglish3(a):
+    if(a == "a"):
+        return "Axe"
+    if(a == "b"):
+        return "Barrel"
+    if(a == "c"):
+        return "Cow"
+    if(a == "d"):
+        return "Dog"
+    if(a == "e"):
+        return "Eye"
+    if(a == "f"):
+        return "Fox"
+    if(a == "g"):
+        return "Gum"
+    if(a == "h"):
+        return "Horse"
+    if(a == "i"):
+        return "Intestine"
+    if(a == "j"):
+        return "Jerk"
+    if(a == "k"):
+        return "Kid"
+    if(a == "l"):
+        return "Lion"
+    if(a == "m"):
+        return "Mouse"
+    if(a == "n"):
+        return "Needle"
+    if(a == "o"):
+        return "Owl"
+    if(a == "p"):
+        return "Pretty"
+    if(a == "q"):
+        return "Quiver"
+    if(a == "r"):
+        return "Rice"
+    if(a == "s"):
+        return "Sheep"
+    if(a == "t"):
+        return "Turkey"
+    if(a == "u"):
+        return "Uncle"
+    if(a == "v"):
+        return "Victor"
+    if(a == "w"):
+        return "Weasel"
+    if(a == "x"):
+        return "Cross"
+    if(a == "y"):
+        return "Yucca"
+    if(a == "z"):
+        return "Zinc"
+    if(a == " "):
+    	return ""
+
+for x in range(1000):
+    randomNumChoice = random.randint(1,3)
+    englishWordList = []
+    navajoWordList = []
+    englishOutput = ""
+    navajoOutput = ""
+    inputedWord = raw_input("Please enter the word you would to translate: ")
+    word = str(inputedWord)
+    print randomNumChoice
+    #print inputedWord
+    for x in range(len(word)):
+        if(randomNumChoice == 1):
+            englishWordList.append(letterToEnglish1(word[x].lower()))
+            navajoWordList.append(letterToNavajo1(word[x].lower()))
+        elif(randomNumChoice == 2):
+            englishWordList.append(letterToEnglish2(word[x].lower()))
+            navajoWordList.append(letterToNavajo2(word[x].lower()))
+        elif(randomNumChoice == 3):
+            englishWordList.append(letterToEnglish3(word[x].lower()))
+            navajoWordList.append(letterToNavajo3(word[x].lower()))
+                
+    for x in englishWordList:
+        englishOutput += str(x)
+        englishOutput += " "
+    
+    counter = 1
+    for x in navajoWordList:
+    	if(x == ""):
+    	    navajoOutput += " "
+    	else:
+            navajoOutput += str(x)
+        #print counter + 1
+        #print len(navajoWordList)
+            if(counter == len(navajoWordList)):
+            #print "if"
+                counter = 0
+                break
+            else:
+            #print "else"
+            	navajoOutput += "-"
+        counter += 1
+    print englishWordList
+    print navajoWordList
+    print 
+    print englishOutput
+    print navajoOutput, "\n"
